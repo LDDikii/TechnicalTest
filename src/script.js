6866b4366e7c8f49b2e90fdade721a73996b7f5d
Vadim <shendakovv@inbox.ru>
2025-04-16 14:37:05 +0300

    Added localStorage func with logic behind it

--- src/script.js
+++ src/script.js
@@ -1,4 +1,10 @@
 const input = document.querySelector(".promocode_form__input");
+const checkbox = document.querySelector(".article_promocode__checkbox");
+const inputBtn = document.querySelector(".checkbox_form__button");
+const successMsg = document.querySelector(".result__success");
+const errMsgCheckbox = document.querySelector(".result__err__checkbox");
+const errMsgNum = document.querySelector(".result__err__num");
+const errMsgNumMissing = document.querySelector('.result__err__num_missing');
 
 const prefixNumber = (str) => {
   if (str === "7") {
@@ -13,7 +19,6 @@ const prefixNumber = (str) => {
   return "7 (";
 };
 
-// ======================================
 input.addEventListener("input", (e) => {
   const value = input.value.replace(/\D+/g, "");
   const numberLength = 11;
@@ -25,7 +30,6 @@ input.addEventListener("input", (e) => {
     result = "+";
   }
 
-  //
   for (let i = 0; i < value.length && i < numberLength; i++) {
     switch (i) {
       case 0:
@@ -45,6 +49,60 @@ input.addEventListener("input", (e) => {
     }
     result += value[i];
   }
-  //
   input.value = result;
+});
+
+inputBtn.addEventListener("click", (e) => {
+    var isChecked = document.getElementById("checkbox").checked;
+
+
+    // successMsg.classList.add('hidden');
+
+    // errMsgCheckbox.classList.remove('show');
+    // errMsgNum.classList.remove('show');
+    // errMsgNumMissing.classList.remove('show');
+    // successMsg.classList.remove('show');
+
+    successMsg.classList.remove('show');
+    successMsg.classList.add('hidden')
+
+
+
+    if (isChecked != true){
+        errMsgCheckbox.classList.remove('hidden');
+        errMsgCheckbox.classList.add('show');
+        return;
+    }
+
+    if (input.value.length != 18){
+        errMsgNumMissing.classList.remove('hidden');
+        errMsgNumMissing.classList.add('show');
+        return;
+    } 
+
+    if (input.value == localStorage.getItem('Number')) {
+        errMsgNum.classList.remove('hidden');
+        errMsgNum.classList.add('show');
+        return;
+    }
+
+    if (isChecked == true && input.value.length == 18){
+        errMsgCheckbox.classList.add('hidden');
+        errMsgCheckbox.classList.remove('show');
+        errMsgNumMissing.classList.remove('show');
+        errMsgNumMissing.classList.add('hidden');
+        errMsgNum.classList.remove('show');
+        errMsgNum.classList.add('hidden');
+
+        localStorage.setItem('Number', input.value);
+        successMsg.classList.remove('hidden');
+        successMsg.classList.add('show');
+        return;
+    }
+
+    // errMsgCheckbox.classList.add('hidden')
+    // errMsgNum.classList.add('hidden');
+    // errMsgNumMissing.classList.add('hidden');
+
+    console.log(input.value);
 });
